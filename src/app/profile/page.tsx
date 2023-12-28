58ba471e55d000d8008afe263b029de5fcffa855
Rostik Wozniak <rostik19wozniak@icloud.com>
2023-12-28 18:23:59 +0100

    add: logout

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,36 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
 export default function ProfilePage(){
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+    async function logout(){
+
+        try {
+            setLoading(true);
+            await axios.get("/api/users/logout");
+            toast.success("Logout success");
+            router.push("/login");
+        } catch (error: any) {
+            console.log(error.message);
+            toast.error(error.message);
+        } finally{
+            setLoading(false);
+        }
+    }
     return(
         <div className="flex flex-col items-center">
             <h1>Profile</h1>
             <hr/>
             <p>Profile page</p>
+            <button 
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 rounded"
+                onClick={logout}
+            >{loading ? "Processing" : "Logout"}</button>
         </div>
     )
 }
