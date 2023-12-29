ab7a2c6cdf720ddb15e1648c68be78329614e2ad
Rostik Wozniak <rostik19wozniak@icloud.com>
2023-12-29 15:58:17 +0100

    style: change to column

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center">
+    <main className="flex flex-col justify-center items-center">
       <h1 className="text-4xl">Main</h1>
+      <Link className="bg-purple-500 px-3 py-1 m-5 rounded" href="/profile">Profile</Link>
     </main>
   )
 }
